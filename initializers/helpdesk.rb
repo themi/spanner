fcf729775482191dfcc01332161ae0201168599f
Tim Hemi (Ops) <tim@reinteractive.net>
2018-06-09 15:49:13 +1000

    secrets moved to ENV

--- initializers/helpdesk.rb
+++ initializers/helpdesk.rb
@@ -1,8 +1,7 @@
 Helpdesk.configure do |c|
   c.zendesk_url = "https://reinteractive.zendesk.com"
-  c.zendesk_username = "tim@reinteractive.net"
-  c.zendesk_api_token = "lQ6oqQCZa0gN9toNopKKCMeLrDUf7HbFHdQQQNN2"
-  c.zendesk_user_id = 294758444
+  c.zendesk_username = ENV["ZENDESK_LOGIN"]
+  c.zendesk_api_token = ENV["ZENDESK_API_TOKEN"]
   c.cache_expire_time = 3600
   c.cache_folder = File.expand_path("cache")
 end
